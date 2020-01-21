c67460a0d760284117ad402d0166ddbf7f9bb1fa
Quentin Fontaine <qfon.dev@gmail.com>
2020-01-21 15:44:51 +0100

    first commit

---
+++ td1/src/main/java/s4/spring/quentin/td1/controller/HelloController.java
@@ -0,0 +1,28 @@
+package s4.spring.quentin.td1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public @ResponseBody String hello(){
+        return "<h1>Bonjour thomas</h1>";
+    }
+
+    @GetMapping("/view/hello")
+    public String viewHello(ModelMap model){
+        model.put("message", "Bonjour tout le monde");
+        return "hello";
+    }
+
+    @GetMapping("/hello/{message}")
+    public String viewHelloDyn(@PathVariable String message){
+        return "hello";
+    }
+
+}
---
+++ td1/src/test/java/s4/spring/quentin/td1/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.spring.quentin.td1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
