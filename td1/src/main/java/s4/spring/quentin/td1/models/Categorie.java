646f13b9ae6e6804f7edefa40592bb1b29d6e3a8
Quentin Fontaine <qfon.dev@gmail.com>
2020-01-28 16:02:40 +0100

    Classe Categorie

---
+++ td1/src/main/java/s4/spring/quentin/td1/models/Categorie.java
@@ -0,0 +1,42 @@
+package s4.spring.quentin.td1.models;
+
+import java.util.ArrayList;
+
+public class Categorie {
+
+    private String libelle;
+
+    private ArrayList<Element> listeElement;
+
+    public Categorie(){
+        libelle = "";
+        listeElement = new ArrayList<Element>();
+    }
+
+    public Categorie(String l){
+        libelle = l;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public ArrayList<Element> getListeElement() {
+        return listeElement;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void ajoutElement(Element e){
+        listeElement.add(e);
+    }
+
+    public void supprimerElement(Element e){
+        if (listeElement.contains(e)){
+            listeElement.remove(e);
+        }
+    }
+
+}
