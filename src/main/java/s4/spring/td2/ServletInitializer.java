9620303a72905756fc962bb19bb6eacee2934a88
Quentin Fontaine <qfon.dev@gmail.com>
2020-02-04 14:00:22 +0100

    td2

---
+++ src/main/java/s4/spring/td2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package s4.spring.td2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Td2Application.class);
+	}
+
+}
--- src/main/java/s4/spring/td2/Td2Application.java
+++ src/main/java/s4/spring/td2/Td2Application.java
@@ -1,13 +1,13 @@
-package s4.spring.quentin.td1;
+package s4.spring.td2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Td1Application {
+public class Td2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Td1Application.class, args);
+		SpringApplication.run(Td2Application.class, args);
 	}
 
 }
