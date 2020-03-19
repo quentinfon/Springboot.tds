2ad05d9d71170645aac8971b314f3751e5cfdb9d
Quentin Fontaine <qfon.dev@gmail.com>
2020-03-19 22:08:53 +0100

    td5

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.org/<username>/<reponame>.svg?branch=master)](https://travis-ci.org/<username>/<reponame>)
---
+++ src/test/java/s4/td/td5/Td5ApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.td.td5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
