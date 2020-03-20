7cb3c6e24e2fee27045318789b745d3be1fb77ab
Quentin Fontaine <qfon.dev@gmail.com>
2020-03-20 11:28:37 +0100

    Entitées

--- src/main/java/s4/td/td5/repositories/ScriptRepository.java
+++ src/main/java/s4/td/td5/repositories/ScriptRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import s4.td.td5.models.Category;
+import s4.td.td5.models.Language;
 import s4.td.td5.models.Script;
 import s4.td.td5.models.User;
 
@@ -19,6 +20,8 @@ public interface ScriptRepository extends JpaRepository<Script, Integer> {
     List<Script> findByOwner(User user);
 
     List<Script> findByCategory(Category category);
+
+    List<Script> findByLanguage(Language language);
 
     @Query("select s from Script s where upper(s.title) like %:search% or upper(s.description) like %:search% or upper(s.content) like %:search%")
     List<Script> recherche(@Param("search")String recherche);
