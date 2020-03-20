dbbde5de2c2012903baf69ff61c113f948fddb02
Quentin Fontaine <qfon.dev@gmail.com>
2020-03-20 15:11:31 +0100

    login

---
+++ src/main/java/s4/td/td5/AppConfig.java
@@ -0,0 +1,9 @@
+package s4.td.td5;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.github.jeemv.springboot.vuejs")
+public class AppConfig {
+}
--- src/main/java/s4/td/td5/controllers/LoginController.java
+++ src/main/java/s4/td/td5/controllers/LoginController.java
@@ -1,11 +1,11 @@
 package s4.td.td5.controllers;
 
+import io.github.jeemv.springboot.vuejs.VueJS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 import s4.td.td5.models.User;
 import s4.td.td5.repositories.UserRepository;
 
@@ -16,22 +16,68 @@ import s4.td.td5.repositories.UserRepository;
 public class LoginController {
 
     @Autowired
+    private VueJS vue;
+
+    @Autowired
     private UserRepository repoUser;
+
+    public static User connectedUser;
 
 
     @GetMapping("/")
     public String viewAccueil(ModelMap model){
 
-        return "accueil";
+        if(connectedUser != null){
+            vue.addData("messageBienvenue", "Bienvenue "+connectedUser.getLogin());
+        }
+        vue.addData("count", 0);
+        model.put("vue", this.vue);
 
+        return "accueil";
+    }
+
+    @GetMapping("/login")
+    public String viewLogin(ModelMap model){
+
+
+        vue.addData("valid", true);
+        vue.addData("login", "");
+        vue.addData("password", "");
+        vue.addMethod("validate", "if(this.$refs.form.validate()){ logForm.submit(); }");
+        vue.addMethod("reset", "this.$refs.form.reset()");
+        model.put("vue", this.vue);
+        return "loginPage";
     }
 
     @GetMapping("/new")
     public String viewNew(ModelMap model){
 
-        User u = new User();
+        if (repoUser.findByLogin("quentin") == null){
+            User u = new User();
+            u.setLogin("quentin");
+            u.setPassword("fontaine");
+
+            repoUser.save(u);
+        }
 
         return "accueil";
+    }
+
+    @PostMapping("/connexion")
+    public RedirectView connexion(@RequestParam String login, @RequestParam String password) {
+
+        connectedUser = repoUser.connexion(login, password);
+
+        return new RedirectView("/");
+
+    }
+
+    @PostMapping("/logout")
+    public RedirectView deconnexion() {
+
+        connectedUser = null;
+        return new RedirectView("/");
+
     }
 
 }
